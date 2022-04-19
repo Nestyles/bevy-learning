fad70c3fcd7f6056ec8d02f24e570d10cc475646
Nestyles <tom.sancho@epitech.eu>
2022-04-19 23:48:35 +0200

    [ADD] Added a timer ressource to prevent hello spam (finished the book)

--- bevy_book/src/main.rs
+++ bevy_book/src/main.rs
@@ -10,9 +10,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_peoples)
-            .add_system(greet_peoples)
-            .add_system(hello_world);
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_peoples)
+            .add_system(greet_peoples);
     }
 
 }
@@ -31,13 +31,13 @@ fn add_peoples(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-// we query everything with a name and we filter with the component Person
-fn greet_peoples(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}", name.0);
-    }
-}
+struct GreetTimer(Timer);
 
-fn hello_world() {
-    println!("hello world !");
+// we query everything with a name and we filter with the component Person
+fn greet_peoples(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}", name.0);
+        }
+    }
 }
